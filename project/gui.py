492fa8240c2bac1ee7cec5563e9ef2a6d1dbfa31
phoenix <196963357+phoenix-dive@users.noreply.github.com>
2025-11-17 12:37:29 -0500

    starting basic gui

---
+++ project/gui.py
@@ -0,0 +1,39 @@
+# Import Module
+from tkinter import *
+
+# create root window
+root = Tk()
+
+# root window title and dimension
+root.title("Packet Tracer Prototype")
+# Set geometry (widthxheight)
+root.geometry('800x600')
+
+# adding a label to the root window
+lbl = Label(root, text = "insert url here...")
+lbl.grid()
+
+# adding Entry Field
+txt = Entry(root, width=10)
+txt.grid(column =1, row =0)
+
+
+# function to display user text when 
+# button is clicked
+def clicked():
+
+    res = "You wrote" + txt.get()
+    lbl.configure(text = res)
+
+def clicked_two():
+    res="among us?"
+    lbl.configure(text = res)
+# button widget with red color text inside
+btn = Button(root, text = "Click me" ,
+             fg = "red", command=clicked_two)
+# Set Button Grid
+btn.grid(column=3, row=1)
+
+# all widgets will be here
+# Execute Tkinter
+root.mainloop()
